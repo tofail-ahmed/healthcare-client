413dab1608b9359624e72821c1042394ceff5a99
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-08 20:18:20 +0600

    navbar added

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Shared/Footer/Footer";
 import Navbar from "@/components/Shared/Navbar/Navbar";
 import React, { ReactNode } from "react";
 
@@ -5,7 +6,8 @@ const CommonLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <Navbar />
-      {children}
+    <div className="min-h-screen">  {children}</div>
+      <Footer/>
     </div>
   );
 };
--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,16 +1,25 @@
-import { Container, Stack, Typography, Box } from "@mui/material";
+import { Container, Stack, Typography, Box, Button } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <Container>
-      <Stack py={2} direction="row" justifyContent="space-between">
-        <Typography variant="h5" component="h1" fontWeight={600}>
-          Health{" "}
+      <Stack py={2} direction="row" justifyContent="space-between" alignItems="center">
+        <Typography variant="h4"  component={Link} href="/" fontWeight={600}>
+          Health
           <Box component="span" color="primary.main">
             Care
           </Box>
         </Typography>
+        <Stack direction="row" gap={2} justifyContent="space-between">
+          <Typography component={Link} href="/consultation" >Consultation</Typography>
+          <Typography component={Link} href="/health-plans"  >Health Plans</Typography>
+          <Typography component={Link} href="/medicine"  >Medicine </Typography>
+          <Typography component={Link} href="/diagnostrics"  >Diagnostrics</Typography>
+          <Typography component={Link} href="/ngos"  >NGO's</Typography>
+        </Stack>
+        <Button component={Link} href="/login">Login</Button>
       </Stack>
     </Container>
   );
