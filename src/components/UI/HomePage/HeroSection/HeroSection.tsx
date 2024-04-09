2a19d1e6d5f70c9b0630e6c081842de68458c941
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-09 14:58:38 +0600

    Creating The Hero Section Segment-2

--- src/components/UI/HomePage/HeroSection/HeroSection.tsx
+++ src/components/UI/HomePage/HeroSection/HeroSection.tsx
@@ -2,40 +2,153 @@ import { Container,Box ,Typography,Button} from '@mui/material';
 import Image from 'next/image';
 import React from 'react'
 import gridBg from '../../../../../public/assets/grid-bg.png'
+import arrow from "../../../../../public/assets/arrow.png";
+import doctor from "../../../../../public/assets/doctor.jpg";
+import doctor1 from "../../../../../public/assets/doctor-ot.jpg";
+import doctor2 from "../../../../../public/assets/doctor2.jpg";
+import stethoscope from "../../../../../public/assets/stethoscope.png";
+
 const HeroSection = () => {
   return (
-    <Container sx={{
-      display:"flex",
-      direction:"row",
-      my:16
-    }}>
-      <Box sx={{
-            flex:1,
-            position:"relative"
-      }}>
-            <Box sx={{
-              position:"absolute",
-              width:"700px",
-              top:"-30px",
-              left:"20px"
-            
-            }}>
-                  <Image src={gridBg} alt="grid" />
-            </Box>
-            <Typography component="h3" variant="h3" fontWeight={600} >Healthier Hearts</Typography>
-            <Typography component="h3" variant="h3" fontWeight={600} >Come From</Typography>
-            <Typography component="h3" variant="h3" fontWeight={600} color="primary.main" >Preventive Care</Typography>
-            <Typography component="h6" variant="h6" fontWeight={400} sx={{
-              width:"50%"
-            }} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores natus incidunt distinctio doloremque fugit sed tempore accusamus magni tempora tenetur?</Typography>
-            <Button>Make Appointment</Button>
-            <Button variant="outlined" sx={{
-              marginLeft:"5px"
-            }}>Contact Us</Button>
+    <Container
+      sx={{
+        display: "flex",
+        direction: "row",
+        my: 16,
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          position: "relative",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            width: "700px",
+            top: "20px",
+            left: "10px",
+          }}
+        >
+          <Image src={gridBg} alt="grid" />
+        </Box>
+        <Typography component="h3" variant="h3" fontWeight={600}>
+          Healthier Hearts
+        </Typography>
+        <Typography component="h3" variant="h3" fontWeight={600}>
+          Come From
+        </Typography>
+        <Typography
+          component="h3"
+          variant="h3"
+          fontWeight={600}
+          color="primary.main"
+        >
+          Preventive Care
+        </Typography>
+        <Typography
+          component="h6"
+          variant="h6"
+          fontWeight={400}
+          my={4}
+          sx={
+            {
+              // width: "50%",
+            }
+          }
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          natus incidunt distinctio doloremque fugit sed tempore accusamus magni
+          tempora tenetur?
+        </Typography>
+        <Button>Make Appointment</Button>
+        <Button
+          variant="outlined"
+          sx={{
+            marginLeft: "5px",
+          }}
+        >
+          Contact Us
+        </Button>
       </Box>
-      <Box>Right</Box>
+      <Box
+        sx={{
+          p: 1,
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          position: "relative",
+          mt: 0,
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "-30px",
+            left: "200px",
+          }}
+        >
+          <Image src={arrow} width={120} height={100} alt="arrow" />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Box>
+            <Image
+              className="rounded-t-full rounded-b-full mt-8"
+              width={240}
+              height={400}
+              src={doctor}
+              alt="doctor"
+            />
+          </Box>
+          <Box>
+            <Image
+              className="rounded-t-full rounded-b-full"
+              width={240}
+              height={300}
+              src={doctor1}
+              alt="doctor1"
+            />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "240px",
+          }}
+        >
+          <Image
+            height={200}
+            width={200}
+            className="rounded-br-[50%] rounded-bl-[50%] rounded-tr-[50%]"
+            src={doctor2}
+            alt="dctr2"
+          />
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "-30px",
+            right: "30px",
+            zIndex: "-1",
+          }}
+        >
+          <Image
+            height={200}
+            width={200}
+
+            src={stethoscope}
+            alt="dctr2"
+          />
+        </Box>
+      </Box>
     </Container>
-  )
-}
+  );
+};
 
 export default HeroSection;
