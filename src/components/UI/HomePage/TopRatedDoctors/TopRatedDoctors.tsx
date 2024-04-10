0d38a69fb444ff947031ee0ed61cd81c7f7e341e
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-10 14:55:04 +0600

    Create Top Rated Doctors Section With Data Fetching

--- src/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors.tsx
+++ src/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors.tsx
@@ -9,19 +9,20 @@ import {
   Card,
   CardMedia,
 } from "@mui/material";
-import React from "react";
+import Image from "next/image";
+import { FaLocationDot } from "react-icons/fa6";
 
 const TopRatedDoctors = async () => {
   const res = await fetch("http://localhost:5000/api/v1/doctor?page=1&limit=3");
   const { data: doctors } = await res.json();
-//   console.log(doctors);
+  //   console.log(doctors);
   return (
     <Box
       sx={{
         my: 10,
         py: 30,
         backgroundColor: "rgba(20,20,20,0.1)",
-        clipPath: "polygon(0 9%, 100% 38%, 100% 100%, 0 76%)", //use clipPath from google
+        clipPath: "polygon(0 1%, 100% 15%, 100% 100%, 0 87%)", //use clipPath from google
       }}
     >
       <Box sx={{ textAlign: "center" }}>
@@ -35,37 +36,75 @@ const TopRatedDoctors = async () => {
           and top-quality surgery facility right here.
         </Typography>
       </Box>
-      <Container  sx={{
-            marginTop:4
-      }}>
-        <Grid container spacing={2}>
+      <Container>
+        <Grid
+          sx={{
+            justifyContent: "center",
+            margin: "30px auto",
+          }}
+          container
+          spacing={4}
+        >
           {doctors.map((doctor: any) => (
             <Grid key={doctor.id} md={4}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                  component="img"
-                  alt="green iguana"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                />
+              <Card
+                className="w-[350px] h-auto"
+                sx={{
+                  margin: "10px 10px",
+                }}
+              >
+                <Box>
+                  <Image
+                    className="w-[350px] h-[300px]"
+                    src={doctor.profilePhoto}
+                    alt="photo"
+                    width={300}
+                    height={100}
+                  />
+                </Box>
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+                    {doctor.name}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
+                    {doctor.qualification}, {doctor.designation}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" my={1}>
+                    Email: {doctor.email}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    <span className="flex items-center ">
+                      <FaLocationDot /> {doctor.address}
+                    </span>
                   </Typography>
                 </CardContent>
-                <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
+                <CardActions
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Button>Book Now</Button>
+                  <Button variant="outlined">View profile</Button>
                 </CardActions>
               </Card>
             </Grid>
           ))}
         </Grid>
+        <Box
+          sx={{
+           
+            textAlign:"center"
+          }}
+        >
+          <Button
+            variant="outlined"
+            sx={{
+              marginTop: "30px",
+            }}
+          >
+            View All
+          </Button>
+        </Box>
       </Container>
     </Box>
   );
