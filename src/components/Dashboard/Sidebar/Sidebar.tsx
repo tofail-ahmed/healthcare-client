1c89b55b36709b48f0f4b4cc788829168d228755
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-17 09:46:12 +0600

    Displaying Menu Items On The Dashboard Sidebar

--- src/components/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -12,36 +12,40 @@ import MailIcon from "@mui/icons-material/Mail";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import Link from "next/link";
 import { GiHospitalCross } from "react-icons/gi";
+import { DrawerItem, UserRole } from "@/types/common";
+import {drawerItems} from "@/utils/drawerItems"
+import SidebarItem from "./SidebarItem";
 const Sidebar = () => {
   return (
     <Box>
       <div>
-        <Typography variant="h4" component={Link} href="/" fontWeight={600} sx={{
-          display:"flex",
-          justifyContent:"center",
-          alignItems:"center",
-          marginTop:"5px"  
-        }}>
-           <Box color="primary.main"> <GiHospitalCross /></Box>
+        <Typography
+          variant="h4"
+          component={Link}
+          href="/"
+          fontWeight={600}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "5px",
+          }}
+        >
+          <Box color="primary.main">
+            {" "}
+            <GiHospitalCross />
+          </Box>
           Health
           <Box component="span" color="primary.main">
             Care
           </Box>
         </Typography>
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
+          {drawerItems("doctor" as UserRole).map((item, index) => (
+        <SidebarItem key={index} index={index} item={item} />
           ))}
         </List>
       </div>
-     
     </Box>
   );
 };
---
+++ src/components/Dashboard/Sidebar/SidebarItem.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import { DrawerItem } from "@/types/common";
+
+
+type IProps={
+      item:DrawerItem;
+      index:number;
+}
+
+
+const SidebarItem = ({item,index}:IProps) => {
+  return (
+    <Link href="/">
+      <ListItem key={index} disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          </ListItemIcon>
+          <ListItemText primary={item.title} />
+        </ListItemButton>
+      </ListItem>
+    </Link>
+  );
+};
+
+export default SidebarItem;
