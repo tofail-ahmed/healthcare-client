c8439d50bdb155e2d5723eddb867322141defb5f
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-18 20:17:10 +0600

    ...

--- src/helpers/axios/axiosBaseQuery.ts
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -6,36 +6,39 @@ import type { AxiosRequestConfig, AxiosError } from 'axios'
 
 export const axiosBaseQuery =
   (
-    { baseUrl }: { baseUrl: string } = { baseUrl: '' }
+    { baseUrl }: { baseUrl: string } = { baseUrl: "" }
   ): BaseQueryFn<
     {
-      url: string
-      method?: AxiosRequestConfig['method']
-      data?: AxiosRequestConfig['data']
-      params?: AxiosRequestConfig['params']
-      headers?: AxiosRequestConfig['headers']
-      meta?:IMeta
+      url: string;
+      method?: AxiosRequestConfig["method"];
+      data?: AxiosRequestConfig["data"];
+      params?: AxiosRequestConfig["params"];
+      headers?: AxiosRequestConfig["headers"];
+      meta?: IMeta;
+      contentType?: string;
     },
     unknown,
     unknown
   > =>
-  async ({ url, method, data, params, headers }) => {
+  async ({ url, method, data, params, headers, contentType }) => {
     try {
       const result = await axios({
         url: baseUrl + url,
         method,
         data,
         params,
-        headers,
-      })
-      return  result 
+        headers: {
+          "Content-Type": contentType || "application/json",
+        },
+      });
+      return result;
     } catch (axiosError) {
-      const err = axiosError as AxiosError
+      const err = axiosError as AxiosError;
       return {
         error: {
           status: err.response?.status,
           data: err.response?.data || err.message,
         },
-      }
+      };
     }
-  }
+  };
---
+++ src/helpers/axios/axiosInstance.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+const instance = axios.create();
+instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.timeout = 60000;
+
+export { instance };
