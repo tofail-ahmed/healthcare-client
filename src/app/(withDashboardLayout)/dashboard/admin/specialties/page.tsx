e0edf823c6cee189a6315918cd221689e0264da9
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-19 11:14:16 +0600

    Start Developing Specialist Feature In Admin Dashboard

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import HealthCareModal from '@/components/Shared/HealthCareModal/HealthCareModal';
+import { Box, Stack,Button,TextField } from '@mui/material';
+
 
 const SpecialtiesPage = () => {
       return (
-            <div>
-                SpecialtiesPage  
-            </div>
+            <Box>
+                <Stack direction="row" justifyContent="space-between" alignItems="center">
+                  {/* <Button>Create Specialities</Button> */}
+                  <HealthCareModal></HealthCareModal>
+                  <TextField size="small" placeholder="Search Specialities"> Search Specialities</TextField>
+                  </Stack> 
+            </Box>
       );
 };
 
