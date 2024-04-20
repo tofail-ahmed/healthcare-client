a47bcab10aa71740e6b5bddf7c25384835b49b34
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-20 21:46:33 +0600

    doctor search functionality added

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -10,11 +10,15 @@ import Loader from '@/components/Shared/Loader/Loader';
 
 
 const DoctorsPage = () => {
-  const [isModalOpen,setIsModalOpen]=useState<boolean>(false);
-  const {data,isLoading}=useGetAllDoctorsQuery({});
-  const handleDelete =  async(id: string) => {
-    console.log(id)
-  
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const query: Record<string, any> = {};
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
+  query["searchTerm"] = searchTerm;
+  const { data, isLoading } = useGetAllDoctorsQuery({ ...query });
+  const handleDelete = async (id: string) => {
+    console.log(id);
+
     // try {
     //   const res = await deleteSpeciality(id).unwrap();
     //   // console.log(res)
@@ -26,17 +30,17 @@ const DoctorsPage = () => {
     // }
   };
   const columns: GridColDef[] = [
-    { field: "name", headerName: "Name", flex:1 },
-    { field: "email", headerName: "Email", flex:1 },
-    { field: "qualification", headerName: "Qualifications", flex:1 },
-    { field: "apointmentFee", headerName: "Fees", flex:1 },
-    
+    { field: "name", headerName: "Name", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "qualification", headerName: "Qualifications", flex: 1 },
+    { field: "apointmentFee", headerName: "Fees", flex: 1 },
+
     {
       field: "action",
       headerName: "Action",
-      flex:1,
-      headerAlign:"center",
-      align:"center",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
       renderCell: ({ row }) => {
         return (
           <Box sx={{}}>
@@ -51,21 +55,25 @@ const DoctorsPage = () => {
       },
     },
   ];
-  const doctors=data?.doctors;
-  console.log(doctors)
-  const meta=data?.meta;
+  const doctors = data?.doctors;
+  // console.log(doctors);
+  const meta = data?.meta;
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Button onClick={()=>setIsModalOpen(true)}>Create a Doctor</Button>
+        <Button onClick={() => setIsModalOpen(true)}>Create a Doctor</Button>
         <DoctorModal open={isModalOpen} setOpen={setIsModalOpen}></DoctorModal>
-        <TextField size="small" placeholder="Search Doctor" />
+        <TextField
+          onChange={(e) => setSearchTerm(e.target.value)}
+          size="small"
+          placeholder="Search Doctor"
+        />
       </Stack>
       <Box sx={{ mt: 2 }}>
         {!isLoading ? (
           <DataGrid rows={doctors} columns={columns} />
         ) : (
-          <Loader/>
+          <Loader />
         )}
       </Box>
     </Box>
